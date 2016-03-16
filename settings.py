c3c8b499d2a107aee0ff3d9ec5177f7fed3e841e
System Administrator <root@Rohits-MacBook-Pro.local>
2016-03-16 23:14:24 +0530

    MMON App First commit

--- settings.py
+++ settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mmon',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -73,11 +74,22 @@ WSGI_APPLICATION = 'mmon.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+#
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mmon',
+        'USER': 'postgres',
+        'PASSWORD': 'edb',
+        'HOST': 'localhost',
+        'PORT': '5444',
     }
 }
 
